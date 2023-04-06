6bacc507c06eacf8ef684ef11b23e31a22008afc
Jessica Schmidt <jessyka_sl@hotmail.com>
2023-04-05 22:09:25 -0300

    inciando com JavaScript

---
+++ script.js
@@ -0,0 +1,35 @@
+// meu primeiro comentário
+
+/* Segundo 
+comentário */
+
+console.log("Hello World!"); //para printar na tela
+
+/*prompt("Qual a sua idade? "); /* aparece uma janelinha para informar 
+a idade */
+
+// Conhecendo e trabalhando com variáveis
+const diaDeNascimento = 18;
+const mesDeNascimento = 05;
+const anoDeNascimento = 1992;
+let texto = "Meu dia de nascimento é: ";
+
+console.log(texto);
+console.log(diaDeNascimento);
+console.log(mesDeNascimento);
+console.log(anoDeNascimento);
+
+//Concatenando
+console.log("Sua data de nascimento é: ",diaDeNascimento, "/", mesDeNascimento, "/", anoDeNascimento);
+console.log(texto + diaDeNascimento + "/" + mesDeNascimento + "/" + anoDeNascimento);
+ 
+//Exemplos:
+
+const primeiroNome = "Jéssica";
+const sobrenome = "Schmidt";
+let minhaIdade = 30;
+let souEstudante = true;
+
+console.log("Meu nome é: " + primeiroNome, sobrenome);
+console.log("Minha idade é: " + minhaIdade);
+console.log("Meu status de estudante é: " + souEstudante);
